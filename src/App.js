0fa8ac980394075fcfe7472dcb4a5217beb10822
Bill Hsiao <School@bill.local>
2019-01-10 08:33:55 -0800

    renders the board in 3 separate rows

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
   render() {
     return (
         <div>
-          <TicTacToe />
           <ResetGame />
+
+          <TicTacToe />
         </div>
     );
   }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,32 @@
 import React, { Fragment } from 'react';
 import Cell from './Cell';
+import './Game.css';
 
 function Game(props) {
+  const cells = props.cells.map((val, index) => {
+    return (
+      <Cell key={val.i} onClick={val.onClick} val={val.val}/>
+    )
+  });
   return (
-    props.cells.map((val, index) => {
+    split(cells, 3).map((val, idx) => {
       return (
-        <Cell key={val.i} onClick={val.onClick} val={val.val}/>
+        <div className={`Row${idx}`} key={idx}>{val}</div>
       )
     })
   )
 }
+
+function split(arr, num) {
+  let temp, idx, newArr = [];
+  for (let i = 0; i < num; i ++) {
+    idx = i * num;
+    temp = arr.slice().splice(idx, 3)
+    newArr.push(temp)
+  }
+  return newArr
+}
+
 
 
 export default Game;
